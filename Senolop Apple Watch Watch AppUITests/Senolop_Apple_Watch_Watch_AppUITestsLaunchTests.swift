9b44786f3ad160e0fdc5a1d76c75fad746f1b1fa
Wallace Silva <ssouza.wallace@gmail.com>
2022-10-06 15:36:10 -0300

    Adds Watch.

---
+++ Senolop Apple Watch Watch AppUITests/Senolop_Apple_Watch_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Senolop_Apple_Watch_Watch_AppUITestsLaunchTests.swift
+//  Senolop Apple Watch Watch AppUITests
+//
+//  Created by Wallace Silva on 06/10/22.
+//
+
+import XCTest
+
+final class Senolop_Apple_Watch_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
