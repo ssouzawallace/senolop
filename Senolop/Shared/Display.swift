74787d9937cc3f8a4655f26b05db3e785b3e1a48
Wallace Silva <ssouza.wallace@gmail.com>
2022-10-07 23:55:36 -0300

    Update Display.swift

--- Senolop/Shared/Display.swift
+++ Senolop/Shared/Display.swift
@@ -33,6 +33,7 @@ struct Display: View {
                     Text("10")
                     Text("12")
                 }
+                .pickerStyle(.segmented)
                 BinariesDisplay(value: Int(calculator.stack.last?.value ?? 0))
             }
         }
