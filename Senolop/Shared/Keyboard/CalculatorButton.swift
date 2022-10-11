8c2f68f9272dd8c914eb7a05c10c3848bed77c26
Wallace Silva <ssouza.wallace@gmail.com>
2022-10-11 15:37:03 -0300

    Update CalculatorButton.swift

--- Senolop/Shared/Keyboard/CalculatorButton.swift
+++ Senolop/Shared/Keyboard/CalculatorButton.swift
@@ -6,63 +6,31 @@
 //
 
 import SwiftUI
-import CoreHaptics
 
 struct CalculatorButton: View {
     let action: () -> Void
-    let labelContent: String
-    
-    private var hapticEngine: CHHapticEngine? {
-        didSet {
-            guard let hapticEngine: CHHapticEngine = hapticEngine else { return }
-            hapticEngine.playsHapticsOnly = true
-            hapticEngine.isAutoShutdownEnabled = false
-            hapticEngine.notifyWhenPlayersFinished { (_) -> CHHapticEngine.FinishedAction in return .leaveEngineRunning }
-            hapticEngine.stoppedHandler = hapticEngineDidStop
-            hapticEngine.resetHandler = hapticEngineDidRecoverFromServerError
-        }
-    }
-    
-    /// Called when the haptic engine fails. Will attempt to restart the haptic engine.
-    private func hapticEngineDidRecoverFromServerError() {
-        print("\(#function)")
-        try? hapticEngine?.start()
-    }
-    
-    /// Called when the haptic engine stops due to an external reason.
-    private func hapticEngineDidStop(reason: CHHapticEngine.StoppedReason) {
-        print("\(#function) -> reason: \(reason)")
-    }
+    let labelContent: String    
+    let expandHorizontally: Bool
+    let backgroundColor: Color?
+    let helper = HapticsHelper.sharedInstance
     
     /// Creates a button that displays a custom label.
     ///
     /// - Parameters:
     ///   - action: The action to perform when the user triggers the button.
     ///   - label: A view that describes the purpose of the button's `action`.
-    public init(action: @escaping () -> Void, labelContent: String) {
+    ///   ///   - expandHorizontally: Does the button fills the container view horizontally. Default is `false`.
+    ///   ///   - backgroundColor: The buttons background color. Default is `nil`.
+    public init(action: @escaping () -> Void, labelContent: String, expandHorizontally: Bool = true, backgroundColor: Color? = nil) {
         self.action = action
         self.labelContent = labelContent
+        self.expandHorizontally = expandHorizontally
+        self.backgroundColor = backgroundColor
     }
     
     func performAction() {
         action()
-        let hapticDict = [
-            CHHapticPattern.Key.pattern: [
-                [CHHapticPattern.Key.event: [
-                    CHHapticPattern.Key.eventType: CHHapticEvent.EventType.hapticTransient,
-                    CHHapticPattern.Key.time: CHHapticTimeImmediate,
-                    CHHapticPattern.Key.eventDuration: 1.0]
-                ]
-            ]
-        ]
-        do {
-            let pattern = try CHHapticPattern(dictionary: hapticDict)
-            let player = try hapticEngine?.makePlayer(with: pattern)
-            try hapticEngine?.start()
-            try player?.start(atTime: 0)
-        } catch {
-            print(error)
-        }
+        helper.vibrate()
     }
     
     var attributedContent: AttributedString {
@@ -74,10 +42,10 @@ struct CalculatorButton: View {
             performAction()
         }, label: {
             Text(attributedContent)
-                .padding()
                 .lineLimit(1)
-                .frame(maxWidth: .infinity)
+                .frame(maxWidth: expandHorizontally ? .infinity : nil)
         })
+        .background(backgroundColor)
         .buttonBorderShape(.roundedRectangle)
         .buttonStyle(.bordered)
     }
