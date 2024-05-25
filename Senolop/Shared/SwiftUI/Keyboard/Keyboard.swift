5cbfe1d18782ed6113d8e0799d677aeb98bc05a0
Wallace Silva <wallace.silva@accedo.tv>
2024-05-25 18:42:38 -0300

    Remove Support to Mac

--- Senolop/Shared/SwiftUI/Keyboard/Keyboard.swift
+++ Senolop/Shared/SwiftUI/Keyboard/Keyboard.swift
@@ -11,57 +11,18 @@ struct Keyboard: View {
     @Binding var calculator: CalculatorProtocol
     
     var body: some View {
-        #if os(macOS)
-        return NumericKeyboard(calculator: $calculator)
-            .touchBar {
-                Button {
-                    calculator.plusPressed()
-                } label: {
-                    Image(systemName: "plus")
-                }
-                Button {
-                    calculator.minusPressed()
-                } label: {
-                    Image(systemName: "minus")
-                }
-                Button {
-                    calculator.multiplyPressed()
-                } label: {
-                    Image(systemName: "multiply")
-                }
-                Button {
-                    calculator.dividePressed()
-                } label: {
-                    Image(systemName: "divide")
-                }
-            }
-            .buttonStyle(CalculatorButtonStyle(proeminent: true))
-            .keyboardShortcut(.cancelAction)
-            .keyboardShortcut(.clear)
-            .keyboardShortcut(.defaultAction)
-            .keyboardShortcut(.delete)
-            .keyboardShortcut(.deleteForward)
-            .keyboardShortcut(.downArrow)
-            .keyboardShortcut(.end)
-            .keyboardShortcut(.escape)
-            .keyboardShortcut(.home)
-            .keyboardShortcut(.leftArrow)
-            .keyboardShortcut(.pageDown)
-            .keyboardShortcut(.pageUp)
-            .keyboardShortcut(.return)
-            .keyboardShortcut(.rightArrow)
-            .keyboardShortcut(.space)
-            .keyboardShortcut(.tab)
-            .keyboardShortcut(.upArrow)
-        #else
-        return NumericKeyboard(calculator: $calculator)
-        #endif
-        
+        NumericKeyboard(
+            calculator: $calculator
+        )
     }
 }
 
 struct Keyboard_Previews: PreviewProvider {
     static var previews: some View {
-        Keyboard(calculator: Binding.constant(RPN()))
+        Keyboard(
+            calculator: Binding.constant(
+                RPN()
+            )
+        )
     }
 }
